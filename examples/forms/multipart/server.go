d554b6162d1beb3dea3c459d09fa6acf0322126f
Kyle Isom <coder@kyleisom.net>
2012-12-17 00:27:44 -0700

    adding multipart example

---
+++ examples/forms/multipart/server.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+        "bytes"
+        "fmt"
+	"github.com/gokyle/webshell"
+        "io"
+        "io/ioutil"
+        "mime/multipart"
+	"net/http"
+        "os"
+)
+
+type Page struct {
+	Processed bool
+	Image     *FormData
+}
+
+type FormData struct {
+	Caption   string
+	ImageFile string
+}
+
+var home_template = webshell.MustCompileTemplate("templates/index.html")
+
+func main() {
+	app := webshell.NewApp("minimal app", "127.0.0.1", "8080")
+	app.AddRoute("/", home)
+        app.StaticRoute("/images/", "images/")
+	app.Serve()
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		getForm(w, r)
+	} else {
+		showForm(w, r)
+	}
+}
+
+func showPage(page Page, w http.ResponseWriter, r *http.Request) {
+	out, err := webshell.BuildTemplate(home_template, page)
+	if err != nil {
+		webshell.Error500(err.Error(), "text/plain", w, r)
+	} else {
+		w.Write(out)
+	}
+}
+
+func showForm(w http.ResponseWriter, r *http.Request) {
+	page := Page{false, nil}
+	showPage(page, w, r)
+}
+
+func getForm(w http.ResponseWriter, r *http.Request) {
+        mp_rdr, err := r.MultipartReader()
+        if err != nil {
+                showPage(Page{false, nil}, w, r)
+                return
+        }
+        var page Page
+        var frm FormData
+        for {
+                part, err := mp_rdr.NextPart()
+                if err == io.EOF {
+                        break
+                }
+                if part.FormName() == "caption" {
+                        br := new(bytes.Buffer)
+                        _, err := io.Copy(br, part)
+                        if err != nil {
+                                break
+                        }
+                        caption := br.Bytes()
+                        frm.Caption = string(caption)
+                } else if part.FormName() == "image" {
+                        fileName := saveTempImage(part)
+                        if fileName == "" {
+                                break
+                        }
+                        frm.ImageFile = fileName
+                }
+        }
+        if frm.ImageFile != "" && frm.Caption != "" {
+                page.Processed = true
+        }
+        page.Image = &frm
+        showPage(page, w, r)
+}
+
+func saveTempImage(part *multipart.Part) string {
+        tmpf, err := ioutil.TempFile("images", "img")
+        if err != nil {
+                return ""
+        }
+
+        _, err = io.Copy(tmpf, part)
+        if err != nil {
+                fmt.Println("[!] ", err.Error())
+                return ""
+        }
+
+        fileName := tmpf.Name() + ".png"
+        tmpf.Close()
+        os.Rename(tmpf.Name(), fileName)
+        return fileName
+}
